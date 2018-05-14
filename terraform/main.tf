7a04fac4f125a02333db975f9641627540d71a18
idilpreetsingh <ds1067@wildcats.unh.edu>
2018-05-14 03:36:40 -0400

    updating final main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,53 +1,39 @@
 terraform {
-
-  backend "gcs" {
-    project = "comp698-ds1067"
-    bucket  = "comp698-ds1067-terraform-state"
-    prefix  = "terraform-state"
-  }
-
-}
-
-provider "google" {
-  region = "us-central1"
-}
-
-//instances
-resource "google_compute_instance_template" "terraform_server"{
-  name         = "terraform_server"
-  project      = "comp698-ds1067"
-  machine_type = "f1-mico"
-
-  // boot disk
-  disk {
-    source_image = "cos-cloud/cos-stable"
-  }
-
-  network_interface {
-    network = "default"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-
-
-resource "google_compute_instance_group_manager" "terraform_group-manager" {
-  name               = "terraform_group-manager"
-  instance_template  = "${google_compute_instance_template.terraform_server.self_link}"
-  base_instance_name = "terraform_group-manager"
-  zone               = "us-central1-f"
-  target_size        = "1"
-}
-
- backend "gcs" {
-   project = "comp698-ds1067"
-   bucket  = "comp698-ds1067-terraform-state"
-   prefix  = "terraform-state"
+backend "gcs" {
+  project = "comp698-ds1067"
+  bucket  = "comp698-ds1067-terraform-state"
+  prefix  = "terraform-state"
  }
 }
+
 provider "google" {
   region = "us-central1"
+}
+
+
+//instances
+resource "google_compute_instance_template" "terraform-webserver"{
+        name = "terraform-webserver"
+        project     = "comp698-ds1067"
+        
+        
+
+        disk {
+        source_image = "cos-cloud/cos-stable"
+        }
+        
+        machine_type         = "f1-mico"
+
+
+        network_interface {
+        network = "default"
+        }     
+}
+
+resource "google_compute_instance_group_manager" "instance_group_manager" {
+name               = "instance-group-manager"
+instance_template  = "${google_compute_instance_template.terraform-webserver.self_link}"
+base_instance_name = "instance-group-manager"
+zone               = "us-central1-f"
+target_size        = "1"
 }
